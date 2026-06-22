802b025866692baae5c82410d526b042b6490e74
VimalanathanThanushan <thanushaan69@gmail.com>
2026-06-22 14:40:30 +0530

    Feat:First Commit with add basic features of backend

---
+++ Back-End/routes/jobRoute.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import { addJob, listJobs, removeJob, singleJob, modifyJob } from '../controllers/jobController.js'
+
+const jobRouter = express.Router();
+
+jobRouter.post('/add', addJob);
+jobRouter.get('/list', listJobs);
+jobRouter.delete('/remove', removeJob);
+jobRouter.get('/single', singleJob);
+jobRouter.put('/modify', modifyJob);
+
+
+
+export default jobRouter;
